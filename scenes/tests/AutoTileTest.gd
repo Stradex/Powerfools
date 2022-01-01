5567f69ac2278406dfba8aab98ef77cb846cb337
Stradex <stradexengine@gmail.com>
2022-01-01 15:18:21 -0300

    creating the copy full tile id data function

--- scenes/tests/AutoTileTest.gd
+++ scenes/tests/AutoTileTest.gd
@@ -2,20 +2,32 @@ extends Node2D
 
 
 onready var auto_tile_img: int = $AutoTileTest.tile_set.find_tile_by_name('autotile1')
-onready var auto_tile_overlay: int = $AutoTileTestOverlay.tile_set.find_tile_by_name('autotile1_overlay')
+#onready var auto_tile_overlay: int = $AutoTileTestOverlay.tile_set.find_tile_by_name('autotile1_overlay')
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	create_overlay_tile()
+	var overlay_tile_id: int = $AutoTileTestOverlay.tile_set.find_tile_by_name($AutoTileTestOverlay.tile_set.tile_get_name(auto_tile_img) + '_overlay')
 	var tile_size: Vector2 = Vector2(64, 64)
 	for x in range(tile_size.x):
 		for y in range(tile_size.y):
 			if $AutoTileTest.get_cellv(Vector2(x, y)) == -1:
 				$AutoTileTestOverlay.set_cellv(Vector2(x, y), -1)
 			else:
-				$AutoTileTestOverlay.set_cellv(Vector2(x, y), auto_tile_overlay)
+				$AutoTileTestOverlay.set_cellv(Vector2(x, y), overlay_tile_id)
 
-	$AutoTileTest.update_bitmask_region();
-	$AutoTileTestOverlay.update_bitmask_region();
+	$AutoTileTest.update_bitmask_region()
+	$AutoTileTestOverlay.update_bitmask_region()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func create_overlay_tile() -> void:
+	var tiles_count: int = $AutoTileTest.tile_set.get_tiles_ids().size()
+	$AutoTileTest.tile_set.create_tile(tiles_count)
+	$AutoTileTest.tile_set.tile_set_texture(tiles_count, $AutoTileTest.tile_set.tile_get_texture(auto_tile_img))
+	$AutoTileTest.tile_set.tile_set_name(tiles_count, $AutoTileTest.tile_set.tile_get_name(auto_tile_img) + "_overlay")
+	var overlay_tile_id: int = $AutoTileTest.tile_set.find_tile_by_name($AutoTileTest.tile_set.tile_get_name(auto_tile_img) + '_overlay')
+	Game.TileSetImporter.copy_autotile_from_to($AutoTileTest.tile_set, auto_tile_img, $AutoTileTest.tile_set, overlay_tile_id, Vector2(0, -1))
+	$AutoTileTestOverlay.tile_set = $AutoTileTest.tile_set
--- scripts/Util/TileSetImporter.gd
+++ scripts/Util/TileSetImporter.gd
@@ -18,6 +18,43 @@ func load_external_tex(path: String) -> ImageTexture:
 	tex_file.close()
 	return imgtex
 
+func copy_autotile_from_to(tile_from: TileSet, tile_id_from: int, tile_to: TileSet, tile_id_to: int, bitmask_subtile_offset = Vector2(0, 0)) -> void:
+	var tile_region: Rect2 = tile_from.tile_get_region(tile_id_from)
+	var auto_tile_size: Vector2 = tile_from.autotile_get_size(tile_id_from)
+	var region_offset: Vector2 = Vector2(bitmask_subtile_offset.x*auto_tile_size.x, bitmask_subtile_offset.y*auto_tile_size.y)
+	var subtiles_size: Vector2 = Vector2(round(tile_region.end.x/auto_tile_size.x), round(tile_region.end.y/auto_tile_size.y))
+	print(tile_region)
+	tile_region.position += region_offset
+	tile_region.end += region_offset
+	print(tile_region)
+	
+	tile_to.tile_set_light_occluder(tile_id_to, tile_from.tile_get_light_occluder(tile_id_from))
+	tile_to.tile_set_material(tile_id_to, tile_from.tile_get_material(tile_id_from))
+	tile_to.tile_set_modulate(tile_id_to, tile_from.tile_get_modulate(tile_id_from))
+	tile_to.tile_set_navigation_polygon(tile_id_to, tile_from.tile_get_navigation_polygon(tile_id_from))
+	tile_to.tile_set_navigation_polygon_offset(tile_id_to, tile_from.tile_get_navigation_polygon_offset(tile_id_from))
+	tile_to.tile_set_normal_map(tile_id_to, tile_from.tile_get_normal_map(tile_id_from))
+	tile_to.tile_set_occluder_offset(tile_id_to, tile_from.tile_get_occluder_offset(tile_id_from))
+	tile_to.tile_set_region(tile_id_to, tile_region)
+	tile_to.tile_set_shapes(tile_id_to, tile_from.tile_get_shapes(tile_id_from).duplicate(true))
+	tile_to.tile_set_texture(tile_id_to, tile_from.tile_get_texture(tile_id_from))
+	tile_to.tile_set_texture_offset(tile_id_to, tile_from.tile_get_texture_offset(tile_id_from))
+	tile_to.tile_set_tile_mode(tile_id_to, tile_from.tile_get_tile_mode(tile_id_from))
+	tile_to.tile_set_z_index(tile_id_to, tile_from.tile_get_z_index(tile_id_from))
+	tile_to.autotile_clear_bitmask_map(tile_id_to)
+	tile_to.autotile_set_size(tile_id_to, tile_from.autotile_get_size(tile_id_from))
+	tile_to.autotile_set_bitmask_mode(tile_id_to, tile_from.autotile_get_bitmask_mode(tile_id_from))
+	tile_to.autotile_set_icon_coordinate(tile_id_to, tile_from.autotile_get_icon_coordinate(tile_id_from))
+	tile_to.autotile_set_spacing(tile_id_to, tile_from.autotile_get_spacing(tile_id_from))
+	print(subtiles_size)
+	for x in range(subtiles_size.x):
+		for y in range(subtiles_size.y):
+			tile_to.autotile_set_bitmask(tile_id_to, Vector2(x, y),  tile_from.autotile_get_bitmask(tile_id_from, Vector2(x,y)))
+			tile_to.autotile_set_light_occluder(tile_id_to, tile_from.autotile_get_light_occluder(tile_id_from, Vector2(x, y)), Vector2(x, y))
+			tile_to.autotile_set_navigation_polygon(tile_id_to, tile_from.autotile_get_navigation_polygon(tile_id_from, Vector2(x, y)), Vector2(x, y))
+			tile_to.autotile_set_subtile_priority(tile_id_to, Vector2(x, y), tile_from.autotile_get_subtile_priority(tile_id_from, Vector2(x, y)))
+			tile_to.autotile_set_z_index(tile_id_to, Vector2(x, y), tile_from.autotile_get_z_index(tile_id_from, Vector2(x, y)))
+
 func append_to_tileset_from_folder(folder_path: String, tile_set: TileSet) -> void:
 	var png_images_in_folder: Array = file_system.list_files_in_directory(folder_path, ".png")
 	var tiles_count: int = tile_set.get_tiles_ids().size()
